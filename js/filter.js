4b67c265485a5623de64233f620376c4c0eaacd5
Deep solanki <46367038+deepsolanki123@users.noreply.github.com>
2021-01-01 17:16:10 +0530

    initial upload of whoim website

---
+++ js/filter.js
@@ -0,0 +1,30 @@
+
+function filter() {
+    var input = document.getElementById('search123');
+    var FilterValue = input.value.toUpperCase();
+    var ul = document.getElementById('search-ul');
+    var li = ul.getElementsByClassName('search-li');
+    for (let i = 0; i < li.length; i++) {
+        var a = li[i].getElementsByTagName('div')[0];
+        if (a.innerText.toUpperCase().indexOf(FilterValue) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function filter1() {
+    var input = document.getElementById('search1234');
+    var FilterValue = input.value.toUpperCase();
+    var ul = document.getElementById('search-ul1');
+    var li = ul.getElementsByClassName('search-li');
+    for (let i = 0; i < li.length; i++) {
+        var a = li[i].getElementsByTagName('div')[0];
+        if (a.innerText.toUpperCase().indexOf(FilterValue) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
